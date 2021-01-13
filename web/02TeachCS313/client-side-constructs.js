2d68fd80971abec5c6b1794dd642c81c30bd7ce6
Dylan Warner <dylanwarner93@gmail.com>
2021-01-13 16:22:03 -0700

    added JQuery

--- web/02TeachCS313/client-side-constructs.js
+++ web/02TeachCS313/client-side-constructs.js
@@ -1,11 +1,13 @@
-function clicked(){
+function clicked() {
     alert("Clicked!");
 }
 
-function changeColor(){
+function changeColor() {
     document.getElementById("div1").style.backgroundColor = document.getElementById("colorName").value;
 }
 
-function changeColorJQuery(){
-   $("#div1").style.backgroundColor = $("#colorName").value; 
-}
+$(document).ready(function () {
+    $("#colorChangeButton").click(function () {
+        $("#div1").style.backgroundColor = $("#colorName").value;
+    });
+});
