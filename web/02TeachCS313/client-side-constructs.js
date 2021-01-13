9bc005060fe3bfe58ab332c48957a6d37adf3ee5
Dylan Warner <dylanwarner93@gmail.com>
2021-01-13 16:13:38 -0700

    added JQuery

--- web/02TeachCS313/client-side-constructs.js
+++ web/02TeachCS313/client-side-constructs.js
@@ -5,3 +5,7 @@ function clicked(){
 function changeColor(){
     document.getElementById("div1").style.backgroundColor = document.getElementById("colorName").value;
 }
+
+function changeColorJQuery(){
+   $("#div1").style.backgroundColor = $("#colorName").value; 
+}
