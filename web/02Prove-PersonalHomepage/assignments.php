b3fadce566d7688339228ce5dc61ad94780efa12
Dylan Warner <dylanwarner93@gmail.com>
2021-01-15 22:19:13 -0700

    initial commit

--- web/02Prove-PersonalHomepage/assignments.php
+++ web/02Prove-PersonalHomepage/assignments.php
@@ -4,12 +4,13 @@
 <head>
     <link rel="stylesheet" href="personal_homepage.css">
     </link>
+    <script src="functions.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title id="pageTitle">Assignments</title>
 </head>
 
-<body>
+<body onload="changePageName()">
 
     <?php require 'header.php'; ?>
 
