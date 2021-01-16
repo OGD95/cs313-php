20a5bedf61109801efdf3cde238cd6a44fc0dda0
Dylan Warner <dylanwarner93@gmail.com>
2021-01-15 21:50:27 -0700

    initial commit

---
+++ web/02Prove-PersonalHomepage/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="personal_homepage.css">
+    </link>
+</head> -->
+
+<body>
+    
+    <div class="header">
+        <?php echo "<h2>Dylan's CS 313 Homepage</h2>" ?>
+        <hr>
+    </div>
+
+    <div class="homepage_menu">
+        <?php '<a id="localLink" href="assignments.html" title="Go to Assignments Page">Assignments</a>' ?>
+    </div>
+
+</body>
+
+</html>
