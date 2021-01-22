9e456293df3730960c662b63a73fb4099be902c7
Dylan Warner <dylanwarner93@gmail.com>
2021-01-22 16:56:43 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove02-Personal_Homepage/personal_homepage.php
+++ web/02Prove-PersonalHomepage/prove02-Personal_Homepage/personal_homepage.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="personal_homepage.css">
+    <link rel="stylesheet" href="/prove02-Personal_Homepage/personal_homepage.css">
     </link>
     <script src="../functions.js"></script>
     <meta charset="UTF-8">
@@ -15,14 +15,14 @@
     <?php require "header.php" ?>
 
     <div class="image_description">
-        <img onmouseover="zoom(this)" onmouseout="zoomOut(this)" class="weddingPhoto" src="images/wedding photo.jpg" alt="Wedding Photo">
+        <img onmouseover="zoom(this)" onmouseout="zoomOut(this)" class="weddingPhoto" src="../images/wedding photo.jpg" alt="Wedding Photo">
         <div class="text">
             <p>Wedding photo with myself, my wife, and my wifes grandmother: Taken 12/21/2019</p>
         </div>
     </div>
 
     <div class="image_description">
-        <img onmouseover="zoom(this)" onmouseout="zoomOut(this)" class="weddingPhoto" src="images/topps-logo.png" alt="Topps' Logo">
+        <img onmouseover="zoom(this)" onmouseout="zoomOut(this)" class="weddingPhoto" src="../images/topps-logo.png" alt="Topps' Logo">
         <div class="text">
             <p>One of my passions is collecting baseball cards. Topps is the main seller of
                 licensed baseball cards in the United States</p>
@@ -30,7 +30,7 @@
     </div>
 
     <div class="image_description">
-        <img onmouseover="zoom(this)" onmouseout="zoomOut(this)" class="weddingPhoto" src="images/panini-logo-small.jpg" alt="Panini America's Logo">
+        <img onmouseover="zoom(this)" onmouseout="zoomOut(this)" class="weddingPhoto" src="../images/panini-logo-small.jpg" alt="Panini America's Logo">
         <div class="text">
             <p>I also enjoy collecting football and basketball cards which are sold mainly by
                 Panini America</p>
