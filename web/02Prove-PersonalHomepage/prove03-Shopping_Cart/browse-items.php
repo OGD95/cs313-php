e0da073d6fca80b2e71fc0623c0583c32ebb5610
Dylan Warner <dylanwarner93@gmail.com>
2021-01-22 16:15:20 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
@@ -2,21 +2,16 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="personal_homepage.css">
-    </link>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignments</title>
+    <title>Browse Items</title>
 </head>
 
 <body>
 
     <?php require 'header.php'; ?>
 
-    <div>
-        <a href="prove03-Shopping_Cart/browse-items.php"></a>
-    </div>
-
+    <p>It Worked!</p>
 </body>
 
 </html>
