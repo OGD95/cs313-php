9066657208ef5bb94faffbe6c1bcfd2fadfb36c5
Dylan Warner <dylanwarner93@gmail.com>
2021-01-23 16:07:54 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/viewCartHelper.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/viewCartHelper.php
@@ -8,6 +8,7 @@ for($i = 0; $i < count($cart); $i++){
     if($cart[$i] == $itemToRemove){
         unset($cart[$i]);
         $cart = array_values($cart);
+        break;
     } else{
         continue;
     }
