bd6817afaf652beecf98c8c9a2943bc1e745d7d9
Dylan Warner <dylanwarner93@gmail.com>
2021-01-22 22:30:33 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 $_SESSION["cart"] = [];
-$cart = $_SESSION["cart"];
-$cart += $_GET["item1"];
-$_SESSION["cart"] = $cart;
-print_r($_SESSION["cart"]);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,6 +26,13 @@ print_r($_SESSION["cart"]);
             <button type="submit" name="item1" value="optic_mega_box">Add to Cart</button>
         </form>
     </div>
+
+    <?php
+    $cart = $_SESSION["cart"];
+    $cart += $_GET["item1"];
+    $_SESSION["cart"] = $cart;
+    print_r($_SESSION["cart"]);
+    ?>
 
     <div class="image_description">
         <img class="photo" src="../images/panini_contenders_hobby_box.png" alt="Panini Contenders Hobby Box">
