72ec3a20484a63f2b376a3824c40e23abbce0229
Dylan Warner <dylanwarner93@gmail.com>
2021-01-23 15:09:26 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/viewCart.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/viewCart.php
@@ -18,15 +18,15 @@ session_start();
     <?php require '../header.php'; ?>
 
     <?php
-    $item1Count = 0;
-    $item2Count = 0;
-    foreach($_SESSION["cart"] as $item){
-        if($item == "optic_mega_box"){
-            $item1Count += 1;
-        }else{
-            $item2Count += 1;
-        }
-    };
+    // $item1Count = 0;
+    // $item2Count = 0;
+    // foreach($_SESSION["cart"] as $item){
+    //     if($item == "optic_mega_box"){
+    //         $item1Count += 1;
+    //     }else{
+    //         $item2Count += 1;
+    //     }
+    // };
     ?>
 
     <div class="image_description" <?php if($item1Count == 0){echo 'id="hideElement"';} ?>>
