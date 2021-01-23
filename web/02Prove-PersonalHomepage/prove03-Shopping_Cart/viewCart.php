8de5f48b73e87c51ecff0836292fa66dfc88222c
Dylan Warner <dylanwarner93@gmail.com>
2021-01-23 14:24:03 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/viewCart.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/viewCart.php
@@ -37,6 +37,19 @@ session_start();
         </div>
     </div>
 
+    <div class="image_description" <?php if($item1Count == 0){echo 'id="hideElement"';} ?>>
+    <img class="photo" src="../images/panini_contenders_hobby_box.png" alt="Panini Contenders Hobby Box">
+        <div class="text">
+        <p><b>2020 Panini Contenders Football Hobby Box -</b> Contains 108 Total Cards Including 5 Guaranteed Autograph 1 of Which Being on Card</p>
+            <span>QTY: <?php echo $item2Count; ?></span>
+        </div>
+    </div>
+
+    <img class="photo" src="../images/panini_contenders_hobby_box.png" alt="Panini Contenders Hobby Box">
+        <div class="text">
+            <p><b>2020 Panini Contenders Football Hobby Box -</b> Contains 108 Total Cards Including 5 Guaranteed Autograph 1 of Which Being on Card</p>
+        </div>
+
     <button><a href="browse-items.php">Continue Shopping</a></button>
 </body>
 
