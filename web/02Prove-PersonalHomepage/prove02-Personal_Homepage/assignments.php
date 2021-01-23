414a4470deec39d5fed44619c83b094dac5c9cd8
Dylan Warner <dylanwarner93@gmail.com>
2021-01-22 17:08:57 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove02-Personal_Homepage/assignments.php
+++ web/02Prove-PersonalHomepage/prove02-Personal_Homepage/assignments.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="personal_homepage.css">
+    <link rel="stylesheet" href="../prove02-Personal_Homepage/personal_homepage.css">
     </link>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
