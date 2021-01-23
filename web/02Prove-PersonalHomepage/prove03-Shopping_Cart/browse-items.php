abe5e9ad78f51d492e5b1cfe9acb88a88f96340a
Dylan Warner <dylanwarner93@gmail.com>
2021-01-22 17:39:34 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
@@ -16,9 +16,18 @@
     <div class="image_description">
         <img class="weddingPhoto" src="../images/optic_mega_box.jpg" alt="Panini Fanatics Optic Mega Box">
         <div class="text">
-            <p>2020 Panini Donruss Optic Fanatics Mega Box - Contains Forty Total Cards Including One Garunteed Autograph and One Mega Box Exclusive Rookie Memorabilia Card</p>
+            <p>2020 Panini Donruss Optic Football Fanatics Mega Box - Contains 40 Total Cards Including 1 Guaranteed Autograph and 1 Mega Box Exclusive Rookie Memorabilia Card</p>
         </div>
     </div>
+
+    <div class="image_description">
+        <img class="weddingPhoto" src="../images/panini_contenders_hobby_box.png" alt="Panini Contenders Hobby Box">
+        <div class="text">
+            <p>2020 Panini Contenders Football Hobby Box - Contains 108 Total Cards Including 5 Guaranteed Autograph 1 of Which Being on Card</p>
+        </div>
+    </div>
+
+
 </body>
 
 </html>
