9fe71afb1f48e28d92b71d20d4cf37f564609e9e
Dylan Warner <dylanwarner93@gmail.com>
2021-01-23 17:07:52 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/checkout.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/checkout.php
@@ -12,8 +12,25 @@
 </head>
 
 <body>
-    <?php require '../header.php'; ?>
-    <form>
+    <?php require '../header.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $_SESSION["address"] = test_input($_POST["address"]);
+        $_SESSION["city"] = test_input($_POST["city"]);
+        $_SESSION["state"] = test_input($_POST["state"]);
+        $_SESSION["zip"] = test_input($_POST["zip"]);
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="address">Address:</label><br>
         <input type="text" id="address" name="address" placeholder="e.g. 123 Nowhere Ave."><br>
         <label for="city">City:</label><br>
@@ -22,6 +39,7 @@
         <input type="text" id="state" name="state" placeholder="e.g. ID"><br>
         <label for="zip">Zip Code:</label><br>
         <input type="text" id="zip" name="zip" placeholder="e.g. 83440"><br>
+        <input type="submit" value="Submit">
     </form>
 </body>
 
