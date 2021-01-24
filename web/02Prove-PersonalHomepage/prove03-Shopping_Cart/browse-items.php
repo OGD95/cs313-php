29ce7525571df2cbb8ec41d19d97a99b0ce1b8e6
Dylan Warner <dylanwarner93@gmail.com>
2021-01-23 18:39:34 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php
@@ -21,8 +21,6 @@ session_start();
         <img class="photo" src="../images/optic_mega_box.jpg" alt="Panini Fanatics Optic Mega Box">
         <div class="text">
             <p><b>2020 Panini Donruss Optic Football Fanatics Mega Box -</b> Contains 40 Total Cards Including 1 Guaranteed Autograph and 1 Mega Box Exclusive Rookie Memorabilia Card</p>
-        </div>
-        <div id="center_button">
             <button onclick="addToCart('optic_mega_box')" type="submit" name="item1" value="optic_mega_box">Add to Cart</button>
         </div>
     </div>
@@ -31,10 +29,10 @@ session_start();
         <img class="photo" src="../images/panini_contenders_hobby_box.png" alt="Panini Contenders Hobby Box">
         <div class="text">
             <p><b>2020 Panini Contenders Football Hobby Box -</b> Contains 108 Total Cards Including 5 Guaranteed Autograph 1 of Which Being on Card</p>
-        </div>
-        <div id="center_button">
             <button id="center_button" onclick="addToCart('contenders_hobby_box')" type="submit" name="item2" value="contenders_hobby_box">Add to Cart</button>
         </div>
+            
+        
 
     </div>
 
