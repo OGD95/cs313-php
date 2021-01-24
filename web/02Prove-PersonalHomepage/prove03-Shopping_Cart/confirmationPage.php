d84188f76ff95b0126179c8771b08b37a4c1dbc8
Dylan Warner <dylanwarner93@gmail.com>
2021-01-23 18:22:01 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/confirmationPage.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/confirmationPage.php
@@ -37,7 +37,8 @@ session_start();
     <h2 class="centerText">Congradulations! Your Items are on Their Way</h2>
     <p class="notice">*Please allow 5 to 7 days for Shipping and Handeling</p>
 
-
+    <p>The Following Items will be sent to <?php echo $_SESSION["address"]?>, <?php echo $_SESSION["city"]?>, <?php echo $_SESSION["state"]?> <?php echo $_SESSION["zip"]?>
+ 
 </body>
 
 </html>
