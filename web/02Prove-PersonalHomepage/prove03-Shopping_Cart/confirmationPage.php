d357e071def2b9b29a3a1cb0a2d56facab7d3801
Dylan Warner <dylanwarner93@gmail.com>
2021-01-23 18:26:03 -0700

    added image folder

--- web/02Prove-PersonalHomepage/prove03-Shopping_Cart/confirmationPage.php
+++ web/02Prove-PersonalHomepage/prove03-Shopping_Cart/confirmationPage.php
@@ -37,7 +37,35 @@ session_start();
     <h2 class="centerText">Congradulations! Your Items are on Their Way</h2>
     <p class="notice">*Please allow 5 to 7 days for Shipping and Handeling</p>
 
-    <p>The Following Items will be sent to <?php echo $_SESSION["address"]?>, <?php echo $_SESSION["city"]?>, <?php echo $_SESSION["state"]?> <?php echo $_SESSION["zip"]?>
+    <p class="centerText">The Following Items will be sent to <?php echo $_SESSION["address"]?>, <?php echo $_SESSION["city"]?>, <?php echo $_SESSION["state"]?> <?php echo $_SESSION["zip"]?>
+
+    <?php
+    $item1Count = 0;
+    $item2Count = 0;
+    foreach($_SESSION["cart"] as $item){
+        if($item == "optic_mega_box"){
+            $item1Count += 1;
+        }else{
+            $item2Count += 1;
+        }
+    };
+    ?>
+    <div class="image_description" <?php if($item1Count == 0){echo 'id="hideElement"';} ?>>
+        <img class="photo" src="../images/optic_mega_box.jpg" alt="Panini Fanatics Optic Mega Box">
+        <div class="text">
+            <p><b>2020 Panini Donruss Optic Football Fanatics Mega Box -</b> Contains 40 Total Cards Including 1 Guaranteed Autograph and 1 Mega Box Exclusive Rookie Memorabilia Card</p>
+            <span>QTY: <?php echo $item1Count; ?></span>
+        </div>
+    </div>
+
+    <div class="image_description" <?php if($item2Count == 0){echo 'id="hideElement"';} ?>>
+    <img class="photo" src="../images/panini_contenders_hobby_box.png" alt="Panini Contenders Hobby Box">
+        <div class="text">
+        <p><b>2020 Panini Contenders Football Hobby Box -</b> Contains 108 Total Cards Including 5 Guaranteed Autograph 1 of Which Being on Card</p>
+            <span>QTY: <?php echo $item2Count; ?></span>
+            <button onclick="removeItem('contenders_hobby_box')" type="submit">Delete Item From Cart</button>
+        </div>
+    </div>
  
 </body>
 
