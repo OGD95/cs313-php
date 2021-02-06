eb76f740e9a2e5d88d1ec2561dba10c8aad02cbe
Dylan Warner <dylanwarner93@gmail.com>
2021-02-05 20:38:25 -0700

    added connections function

--- web/02Prove-PersonalHomepage/prove02-Personal_Homepage/assignments.php
+++ web/02Prove-PersonalHomepage/prove02-Personal_Homepage/assignments.php
@@ -15,7 +15,7 @@
 
     <div>
         <a href="../../02Prove-PersonalHomepage/prove03-Shopping_Cart/browse-items.php">Prove 03 - PHP Shipping Cart</a>
-        <br\>
+        </br>
         <a href="../../02Prove-PersonalHomepage/prove05-PHP_Data_Access/practice.php">Prove 05 - PHP Data Access</a>
     </div>
 
