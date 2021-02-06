e398bc60db32a7b52c50f6388dd4c77518314670
Dylan Warner <dylanwarner93@gmail.com>
2021-02-05 22:07:32 -0700

    added connections function

--- web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/user_info.php
+++ web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/user_info.php
@@ -29,7 +29,12 @@ session_start();
         echo '<br/>';
     }
 
-    echo $_GET['user'];
+    echo 'SELECT s.sport, c.card_condition, m.manufacturer, cl.athlete_first_name, cl.athlete_last_name. cl.description
+    FROM card_listings cl
+    INNER JOIN sports s ON cl.sport_id = s.sport_id
+    INNER JOIN conditions c ON cl.condition_id = c.condition_id
+    INNER JOIN manufacturer m ON cl.manufaturer_id = c.manufacturer_id
+    WHERE cl.account_id = ' . $_GET['user'];
 
     foreach ($db->query('SELECT s.sport, c.card_condition, m.manufacturer, cl.athlete_first_name, cl.athlete_last_name. cl.description
                         FROM card_listings cl
