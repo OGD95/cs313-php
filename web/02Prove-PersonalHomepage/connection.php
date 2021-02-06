affc3f0b98f1580b9b31d0ad57d25421dc6b6e95
Dylan Warner <dylanwarner93@gmail.com>
2021-02-05 19:18:48 -0700

    added connections function

--- web/02Prove-PersonalHomepage/connection.php
+++ web/02Prove-PersonalHomepage/connection.php
@@ -30,15 +30,11 @@ if (empty($dbUrl)) {
 
 $dbopts = parse_url($dbUrl);
 
-print "<p>$dbUrl</p>\n\n";
-
 $dbHost = $dbopts["host"];
 $dbPort = $dbopts["port"];
 $dbUser = $dbopts["user"];
 $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
-
-print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
 
 try {
  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
@@ -46,11 +42,6 @@ try {
 catch (PDOException $ex) {
  print "<p>error: $ex->getMessage() </p>\n\n";
  die();
-}
-
-foreach ($db->query('SELECT now()') as $row)
-{
- print "<p>$row[0]</p>\n\n";
 }
 
 return $db;
