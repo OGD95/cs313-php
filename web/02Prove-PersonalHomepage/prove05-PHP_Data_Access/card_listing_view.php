78061e7f64fd75f5ef2f635eab956fd3a2b78480
Dylan Warner <dylanwarner93@gmail.com>
2021-02-05 23:10:04 -0700

    added connections function

--- web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/card_listing_view.php
+++ web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/card_listing_view.php
@@ -20,29 +20,31 @@ session_start();
     require '../connection.php';
     ?>
 
-    <?php
+    <div id="card_listings">
+        <?php
 
-    $db = connectToDB();
+        $db = connectToDB();
 
-    foreach ($db->query('SELECT a.username, s.sport, c.card_condition, m.manufacturer, cl.athlete_first_name, cl.athlete_last_name, cl.description
+        foreach ($db->query('SELECT a.username, s.sport, c.card_condition, m.manufacturer, cl.athlete_first_name, cl.athlete_last_name, cl.description
                         FROM card_listings cl
                         INNER JOIN accounts a ON cl.account_id = a.account_id
                         INNER JOIN sports s ON cl.sport_id = s.sport_id
                         INNER JOIN conditions c ON cl.condition_id = c.condition_id
                         INNER JOIN manufacturers m ON cl.manufacturer_id = m.manufacturer_id') as $row) {
-        echo ' Seller: ' . $row['username'];
-        echo '<br/>';
-        echo ' Sport: ' . $row['sport'];
-        echo '<br/>';
-        echo ' Card Condition: ' . $row['card_condition'];
-        echo '<br/>';
-        echo ' Manufacturer: ' . $row['manufacturer'];
-        echo '<br/>';
-        echo ' Athlete Name: ' . $row['athlete_first_name'] . ' ' . $row['athlete_last_name'];
-        echo '<br/>';
-        echo '<hr>';
-    }
+            echo ' Seller: ' . $row['username'];
+            echo '<br/>';
+            echo ' Sport: ' . $row['sport'];
+            echo '<br/>';
+            echo ' Card Condition: ' . $row['card_condition'];
+            echo '<br/>';
+            echo ' Manufacturer: ' . $row['manufacturer'];
+            echo '<br/>';
+            echo ' Athlete Name: ' . $row['athlete_first_name'] . ' ' . $row['athlete_last_name'];
+            echo '<br/>';
+            echo '<hr>';
+        }
 
-    ?>
+        ?>
+    </div>
 
 </body>
