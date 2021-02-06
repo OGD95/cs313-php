469dd474d112b513b940df470d21fed7627ec190
Dylan Warner <dylanwarner93@gmail.com>
2021-02-05 21:22:09 -0700

    added connections function

--- web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/user_list.php
+++ web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/user_list.php
@@ -25,7 +25,7 @@ session_start();
     $db = connectToDB();
 
     foreach ($db->query('SELECT first_name , last_name FROM accounts ORDER BY last_name, first_name') as $row) {
-        echo 'Seller: ' . '<a href="user_info.php?lname=' . $row['last_name'] . '&fname=' . $row['first_name'] . '"';
+        echo 'Seller: ' . '<a href="user_info.php?lname=' . $row['last_name'] . '&fname=' . $row['first_name'] . '">' . $row['last_name'] . ', ' . $row['first_name'] . '<a>';
         echo '<br/>';
     }
 
