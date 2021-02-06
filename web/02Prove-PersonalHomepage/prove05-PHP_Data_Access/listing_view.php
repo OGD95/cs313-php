cb521a53e4e9e95cde1f57cda00b36faff4a1ee0
Dylan Warner <dylanwarner93@gmail.com>
2021-02-06 00:59:15 -0700

    added connections function

--- web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/listing_view.php
+++ web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/listing_view.php
@@ -24,7 +24,7 @@ session_start();
 
     $db = connectToDB();
 
-    foreach ($db->query('SELECT a.username, s.sport, c.card_condition, m.manufacturer, cl.athlete_first_name, cl.athlete_last_name, cl.description, cl.listing_id
+    foreach ($db->query('SELECT a.username, s.sport, c.card_condition, m.manufacturer, cl.athlete_first_name, cl.athlete_last_name, cl.description, cl.listing_id, cl.account_id
                         FROM card_listings cl
                         INNER JOIN accounts a ON cl.account_id = a.account_id
                         INNER JOIN sports s ON cl.sport_id = s.sport_id
