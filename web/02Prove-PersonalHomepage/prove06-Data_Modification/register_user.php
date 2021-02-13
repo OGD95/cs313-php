a46af87030c68561fff8a3f05ed1630d43f8530a
Dylan Warner <dylanwarner93@gmail.com>
2021-02-13 02:13:41 -0700

    changes

--- web/02Prove-PersonalHomepage/prove06-Data_Modification/register_user.php
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/register_user.php
@@ -6,14 +6,16 @@ $password = $_POST['password'];
 $username = $_POST['username'];
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lasName'];
+$shippingAddress = '7210 Oakberry Way';
 
-$query = 'INSERT INTO accounts(password, username, first_name, last_name)
-          VALUES (:password, :username, :firstName, :lastName)';
+$query = 'INSERT INTO accounts(password, username, shipping_address, first_name, last_name)
+          VALUES (:password, :username, :shipping_address, :firstName, :lastName)';
 
 $statement = $db->prepare($query);
 
 $statement->bindValue(':password', $password);
 $statement->bindValue(':username', $username);
+$statement->bindValue(':shipping_address', $shippingAddress);
 $statement->bindValue(':firstName', $firstName);
 $statement->bindValue(':lastName', $lastName);
 
