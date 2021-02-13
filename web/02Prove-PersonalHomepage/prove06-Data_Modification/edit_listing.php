e0ac5d622e734bb5e8102082da548d1a8f685c40
Dylan Warner <dylanwarner93@gmail.com>
2021-02-12 22:41:53 -0700

    changes

---
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/edit_listing.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="../prove02-Personal_Homepage/personal_homepage.css">
+    </link>
+    <script src="../functions.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listing View</title>
+</head>
+
+<body>
+
+    <?php
+    require '../header.php';
+    require '../connection.php';
+    ?>
+</body>
