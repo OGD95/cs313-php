9db5e9bac06f26c1f2b7a02e9f680aa424ce0db8
Dylan Warner <dylanwarner93@gmail.com>
2021-02-12 23:08:57 -0700

    changes

--- web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/listing_view.php
+++ web/02Prove-PersonalHomepage/prove05-PHP_Data_Access/listing_view.php
@@ -49,7 +49,7 @@ session_start();
         '<a href="user_info.php?user=' . $row['account_id'] . '">' . $row['last_name'] . ', ' . $row['first_name'] . '<a>';
     ?>
 
-    <form action="../prove06-Data_Modification/edit_listing.php?listing_id=" <?php echo $_GET['listing_id'] ?>>
+    <form action="../prove06-Data_Modification/edit_listing.php?listing_id=<?php echo $_GET['listing_id'] ?>" method="GET">
         <input type="submit" value="Edit Listing">
     </form>
 
