e15ce74f8f9db3620a8b941572c2b63ba135ac18
Dylan Warner <dylanwarner93@gmail.com>
2021-02-12 19:03:58 -0700

    changes

--- web/02Prove-PersonalHomepage/prove06-Data_Modification/create_listing.php
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/create_listing.php
@@ -24,27 +24,33 @@ $db = connectToDB();
     <form id="cardListingForm" action="insertListing.php" method="POST">
         <label for="athlete_first_name">Athlete's First Name:</label>
         <input type="text" id="athlete_first_name" name="athlete_first_name"></input>
-        
+
         </br> </br>
 
         <lable for="athlete_last_name">Athlete's Last Name:</lable>
         <input type="text" id="athlete_last_name" name="athlete_last_name"></input>
-        
+
         </br> </br>
 
         <?php
         $statement = $db->prepare('SELECT condition_id, card_condition FROM conditions');
         $statement->execute();
 
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-        {
-            $id = $row['condition_id'];
-            $condition = $row['card_condition'];
+        try {
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                $id = $row['condition_id'];
+                $condition = $row['card_condition'];
 
-            echo "<input type='checkbox' name='chkTopics[]' id='chkTopics$id' value='$id'>";
-            echo "<label for='chkTopics$id'>$name</label><br />";
+                echo "<input type='checkbox' name='chkTopics[]' id='chkTopics$id' value='$id'>";
+                echo "<label for='chkTopics$id'>$name</label><br />";
 
-            echo "\n";
+                echo "\n";
+            }
+        } catch (PDOException $ex) {
+            // Please be aware that you don't want to output the Exception message in
+            // a production environment
+            echo "Error connecting to DB. Details: $ex";
+            die();
         }
         ?>
 
