e4614cbb320b259242791f21499f33ca8dff25c7
Dylan Warner <dylanwarner93@gmail.com>
2021-02-13 03:13:50 -0700

    changes

--- web/02Prove-PersonalHomepage/prove06-Data_Modification/delete_listing.php
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/delete_listing.php
@@ -13,4 +13,6 @@ $statement = $db->prepare($query);
 $statement->bindValue(':listing_id', $listing_id);
 
 $statement->execute();
+
+header("Location: ../prove05-PHP_Data_Access/card_listings_view.php?sort_option=username");
 ?>
