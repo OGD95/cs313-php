168f7ecddfa8754a6250609dfd9f849f51d31dbc
Dylan Warner <dylanwarner93@gmail.com>
2021-02-13 00:08:01 -0700

    changes

--- web/02Prove-PersonalHomepage/prove06-Data_Modification/edit_listing.php
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/edit_listing.php
@@ -10,7 +10,7 @@ session_start();
     <script src="../functions.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listing View</title>
+    <title>Edit Listing</title>
 </head>
 
 <body>
@@ -18,5 +18,35 @@ session_start();
     <?php
     require '../header.php';
     require '../connection.php';
+    $db = connectToDB();
+
+    $statement = $db->prepare('SELECT a.username, s.sport, c.card_condition, m.manufacturer, cl.athlete_first_name, cl.athlete_last_name, cl.description, cl.listing_id, cl.account_id
+    FROM card_listings cl
+    INNER JOIN accounts a ON cl.account_id = a.account_id
+    INNER JOIN sports s ON cl.sport_id = s.sport_id
+    INNER JOIN conditions c ON cl.condition_id = c.condition_id
+    INNER JOIN manufacturers m ON cl.manufacturer_id = m.manufacturer_id
+    WHERE listing_id = ' . $_GET["listing_id"]);
+
+    $statement->execute();
+
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $username = $row['username'];
+        $sport = $row['sport'];
+        $condition = $row['card_condition'];
+        $manufacturer = $row['manufacturer'];
+        $athleteFirstName = $row['athlete_first_name'];
+        $athleteLastName = $row['athlete_last_name'];
+        $description = $row['description'];
+        $userId = $row['account_id'];
+    }
     ?>
+
+    <form id="editListingForm" action="change_listing.php" method="POST">
+        <label for="athlete_first_name">Athlete's First Name:</label>
+        </br>
+        <input type="text" id="athlete_first_name" name="athlete_first_name"><?php echo $athleteFirstName ?></input>
+    </form>
+
+
 </body>
