7250eccc1c5a663716f661b9cd7f86e110eb361c
Dylan Warner <dylanwarner93@gmail.com>
2021-02-12 20:55:01 -0700

    changes

--- web/02Prove-PersonalHomepage/prove06-Data_Modification/insert_listing.php
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/insert_listing.php
@@ -11,12 +11,21 @@ $sport_id = $_POST['radioSport'];
 $manufacturer_id = $_POST['radioManufacturer'];
 $description = $_POST['listingDescription'];
 
+echo $athleteFirstName;
+echo $athleteLastName;
+echo $condition_id;
+echo $sport_id;
+echo $manufacturer_id;
+echo $description;
+
 $statement = $db->prepare('SELECT account_id FROM accounts WHERE username=' . $_POST['seller']);
 $statement->execute();
 
 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
     $seller = $row['account_id'];
 }
+
+echo $seller;
 
 $query = 'INSERT INTO card_listings(account_id, sport_id, condition_id, manufacturer_id, athlete_first_name, athlete_last_name, description) 
           VALUES (:account_id, :sport_id, :condition_id, :manufacturer_id, :athlete_first_name, :athlete_last_name, :description)';
@@ -31,11 +40,11 @@ $statement->bindValue(':athlete_first_name', $athleteFirstName);
 $statement->bindValue(':athlete_last_name', $athleteLastName);
 $statement->bindValue(':description', $description);
 
-$statement->execute();
+// $statement->execute();
 
 $listing_id = $db->lastInsertId("card_listings_listing_id_seq");
 
-header("Location: ../prove05-PHP_Data_Access/listing_view.php?listing_id=" . $listing_id);
+// header("Location: ../prove05-PHP_Data_Access/listing_view.php?listing_id=" . $listing_id);
 
 die();
 ?>
