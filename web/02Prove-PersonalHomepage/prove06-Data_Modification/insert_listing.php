f1aa4ba013bdb5106c35ff4368b557832a32ee69
Dylan Warner <dylanwarner93@gmail.com>
2021-02-12 20:44:27 -0700

    changes

--- web/02Prove-PersonalHomepage/prove06-Data_Modification/insert_listing.php
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/insert_listing.php
@@ -1,41 +1,41 @@
 <?php
-// require '../connection.php';
-// $db = connectToDB();
+require '../connection.php';
+$db = connectToDB();
 
 
 
-// $athleteFirstName = $_POST['athlete_first_name'];
-// $athleteLastName = $_POST['athlete_last_name'];
-// $condition_id = $_POST['radioCondition'];
-// $sport_id = $_POST['radioSport'];
-// $manufacturer_id = $_POST['radioManufacturer'];
-// $description = $_POST['listingDescription'];
+$athleteFirstName = $_POST['athlete_first_name'];
+$athleteLastName = $_POST['athlete_last_name'];
+$condition_id = $_POST['radioCondition'];
+$sport_id = $_POST['radioSport'];
+$manufacturer_id = $_POST['radioManufacturer'];
+$description = $_POST['listingDescription'];
 
-// $statement = $db->prepare('SELECT account_id FROM accounts WHERE username=' . $_POST['seller']);
-// $statement->execute();
+$statement = $db->prepare('SELECT account_id FROM accounts WHERE username=' . $_POST['seller']);
+$statement->execute();
 
-// while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-//     $seller = $row['account_id'];
-// }
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $seller = $row['account_id'];
+}
 
-// $query = 'INSERT INTO card_listings(account_id, sport_id, condition_id, manufacturer_id, athlete_first_name, athlete_last_name, description) 
-//           VALUES (:account_id, :sport_id, :condition_id, :manufacturer_id, :athlete_first_name, :athlete_last_name, :description)';
+$query = 'INSERT INTO card_listings(account_id, sport_id, condition_id, manufacturer_id, athlete_first_name, athlete_last_name, description) 
+          VALUES (:account_id, :sport_id, :condition_id, :manufacturer_id, :athlete_first_name, :athlete_last_name, :description)';
 
-// $statement = $db->prepare($query);
+$statement = $db->prepare($query);
 
-// $statement->bindValue(':account_id', $seller);
-// $statement->bindValue(':sport_id', $sport_id);
-// $statement->bindValue(':condition_id', $condition_id);
-// $statement->bindValue(':manufacturer_id', $manufacturer_id);
-// $statement->bindValue(':athlete_first_name', $athleteFirstName);
-// $statement->bindValue(':athlete_last_name', $athleteLastName);
-// $statement->bindValue(':description', $description);
+$statement->bindValue(':account_id', $seller);
+$statement->bindValue(':sport_id', $sport_id);
+$statement->bindValue(':condition_id', $condition_id);
+$statement->bindValue(':manufacturer_id', $manufacturer_id);
+$statement->bindValue(':athlete_first_name', $athleteFirstName);
+$statement->bindValue(':athlete_last_name', $athleteLastName);
+$statement->bindValue(':description', $description);
 
-// $statement->execute();
+$statement->execute();
 
-// $listing_id = $db->lastInsertId("listing_id_seq");
+$listing_id = $db->lastInsertId("listing_id_seq");
 
-header("Location: ../prove05-PHP_Data_Access/listing_view.php?listing_id=2");
+header("Location: ../prove05-PHP_Data_Access/listing_view.php?listing_id=" . $listing_id);
 
 die();
 ?>
