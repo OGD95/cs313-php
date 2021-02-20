924559c81259b2eb5f497fe0a2d7153c3509d07a
Dylan Warner <dylanwarner93@gmail.com>
2021-02-20 09:40:26 -0700

    week 07 team activity

--- web/02Prove-PersonalHomepage/header.php
+++ web/02Prove-PersonalHomepage/header.php
@@ -1,11 +1,16 @@
-
+<?php 
+session_start();
+if(!isset($_SESSION['current_user_id']) && !isset($_SESSION['current_user_username']) && substr($_SERVER['REQUEST_URI'], -11) != 'sign-in.php')
+{
+    header("Location: ../prove06-Data_Modification/sign-in.php");
+    die();
+}
+?>
 <body>
     <div class="header">
         <?php echo "<h2>Dylan's CS 313 Homepage</h2>" ?>
         <hr>
     </div>
-
-    <?php echo substr($_SERVER['REQUEST_URI'], -11); ?>
 
     <div class="homepage_menu">
         <?php echo '<a id="localLink" href="../prove02-Personal_Homepage/personal_homepage.php" title="Go to Homepage">Homepage</a>' ?>
