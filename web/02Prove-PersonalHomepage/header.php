99352aac1563fa1a4239cff3c68d16dedb6400be
Dylan Warner <dylanwarner93@gmail.com>
2021-02-20 00:13:06 -0700

    week 07 team activity

--- web/02Prove-PersonalHomepage/header.php
+++ web/02Prove-PersonalHomepage/header.php
@@ -1,7 +1,6 @@
 <body>
-    <button onclick="unset($_SESSION['current_user_id'], $_SESSION['current_user_username'])"></button>
-
     <div class="header">
+        <button onclick="unset($_SESSION['current_user_id'], $_SESSION['current_user_username'])">Logout</button>
         <?php echo "<h2>Dylan's CS 313 Homepage</h2>" ?>
         <hr>
     </div>
