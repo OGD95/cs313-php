b5f3bd482abd4554f957fc24680976fe7b1063d2
Dylan Warner <dylanwarner93@gmail.com>
2021-02-19 22:21:55 -0700

    week 07 team activity

--- web/02Prove-PersonalHomepage/prove06-Data_Modification/verify_info.php
+++ web/02Prove-PersonalHomepage/prove06-Data_Modification/verify_info.php
@@ -6,18 +6,19 @@ $db = connectToDB();
 $password = $_POST['password'];
 $username = $_POST['username'];
 
-$statement = $db->prepare('SELECT password, accountid FROM accounts WHERE username =' . '\'' . $username . '\'');
+$statement = $db->prepare('SELECT password, account_id FROM accounts WHERE username =' . $username);
 $statement->execute();
 
 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
     $hashedPassword = $row['password'];
-    $accountId = $row['accountid'];
+    $accountId = $row['account_id'];
 }
 
 if(password_verify($password, $hashedPassword)){
     $_SESSION['current_user_id'] = $accountId;
     $_SESSION['current_user_username'] = $username;
     header('Location: welcome.php');
+    die();
 }else{
     header('Location: sign-in.php');
     die();
