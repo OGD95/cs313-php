93ddb12c3218695526713582a390118deafd7b3f
Dylan Warner <dylanwarner93@gmail.com>
2021-02-20 13:23:41 -0700

    week 07 team activity

--- web/02Prove-PersonalHomepage/header.php
+++ web/02Prove-PersonalHomepage/header.php
@@ -1,19 +1,24 @@
-<?php 
+<?php
 session_start();
-if(!isset($_SESSION['current_user_id']) && !isset($_SESSION['current_user_username']) && substr($_SERVER['REQUEST_URI'], -11) != 'sign-in.php' && substr($_SERVER['REQUEST_URI'], -11) != 'sign-up.php')
-{
+if (!isset($_SESSION['current_user_id']) && !isset($_SESSION['current_user_username']) && substr($_SERVER['REQUEST_URI'], -11) != 'sign-in.php' && substr($_SERVER['REQUEST_URI'], -11) != 'sign-up.php') {
     header("Location: ../prove06-Data_Modification/sign-in.php");
     die();
 }
 ?>
+
 <body>
-    <?php 
-    if(isset($_SESSION['current_user_id']) && isset($_SESSION['current_user_username'])){
+    <?php
+    if (isset($_SESSION['current_user_id']) && isset($_SESSION['current_user_username'])) {
         echo '<a href="../prove06-Data_Modification/signout.php">Logout</a>';
     }
     ?>
-    
+
     <div class="header">
+        <?php
+        if (isset($_SESSION['current_user_id']) && isset($_SESSION['current_user_username'])) {
+            echo '<a href="../prove06-Data_Modification/signout.php">Logout</a>';
+        }
+        ?>
         <?php echo "<h2>Dylan's CS 313 Homepage</h2>" ?>
         <hr>
     </div>
@@ -23,4 +28,4 @@ if(!isset($_SESSION['current_user_id']) && !isset($_SESSION['current_user_userna
         <?php echo "|" ?>
         <?php echo '<a id="localLink" href="../prove02-Personal_Homepage/assignments.php" title="Go to Assignments Page">Assignments</a>' ?>
     </div>
-</body>    
+</body>
