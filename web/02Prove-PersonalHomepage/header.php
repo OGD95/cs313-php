aa08f115447f95edfc1308854dd05ffa076984dd
Dylan Warner <dylanwarner93@gmail.com>
2021-02-20 09:39:14 -0700

    week 07 team activity

--- web/02Prove-PersonalHomepage/header.php
+++ web/02Prove-PersonalHomepage/header.php
@@ -5,7 +5,7 @@
         <hr>
     </div>
 
-    <?php echo substr($_SERVER['REQUEST_URI'], -10); ?>
+    <?php echo substr($_SERVER['REQUEST_URI'], -11); ?>
 
     <div class="homepage_menu">
         <?php echo '<a id="localLink" href="../prove02-Personal_Homepage/personal_homepage.php" title="Go to Homepage">Homepage</a>' ?>
